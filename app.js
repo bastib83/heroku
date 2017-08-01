43950ca314be73a0083539b3d9787be14b17a948
bastib83 <bastib83@gmail.com>
2017-08-01 23:32:02 +0200

    mLab MongoDB example1

--- app.js
+++ app.js
@@ -5,6 +5,33 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var lessMiddleware = require('less-middleware');
+var MongoClient = require('mongodb').MongoClient  
+    , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://admin:pa22w0rd@ds062919.mlab.com:62919/mrj1ngles';
+
+var findDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('user');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+}
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+
+    findDocuments(db, function() {
+      db.close();
+    });
+}); 
 
 var index = require('./routes/index');
 var users = require('./routes/users');
