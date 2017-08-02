13ed2c0589946a7c5974f9ba234396eb6b99edd0
bastib83 <bastib83@gmail.com>
2017-08-02 12:39:47 +0200

    added data from db in view/routes

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,29 @@
 var express = require('express');
 var router = express.Router();
-var MongoClient = require('mongodb').MongoClient  
-    , assert = require('assert');
+var MongoClient = require('mongodb').MongoClient
+        , assert = require('assert');
 
 // Connection URL
 var url = 'mongodb://admin:pa22w0rd@ds062919.mlab.com:62919/mrj1ngles';
 
-var findDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('user');
-  // Find some documents
-  collection.find({}).toArray(function(err, docs) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(docs)
-    callback(docs);
-  });
-}
-
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    
-    MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
+router.get('/', function (req, res, next) {
 
+    MongoClient.connect(url, function (err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
 
-    findDocuments(db, function() {
-      db.close();
+        // Get the documents collection
+        var collection = db.collection('user');
+        // Find some documents
+        collection.find({}).toArray(function (err, docs) {
+            assert.equal(err, null);
+            console.log("Found the following records");
+            console.log(docs)
+            res.render('index', {title: 'Home', data: docs});
+        });
+        db.close();
     });
-}); 
-
-    
-  res.render('index', { title: 'Home' });
 });
 
 module.exports = router;
