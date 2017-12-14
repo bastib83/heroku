32f656fc2121ca54665b4283d1d2539d805e5a63
DESKTOP-4TVTULT\MrJingles <bastib83@gmail.com>
2017-12-14 21:12:17 +0100

    added local infoObjects File added switching between db or filestream

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,42 @@
 var express = require('express');
 var router = express.Router();
+//until DB is connected
+var fs = require("fs");
+//end
+
 var MongoClient = require('mongodb').MongoClient
         , assert = require('assert');
-
+// mongo ds062919.mlab.com:62919/mrj1ngles -u <dbuser> -p <dbpassword> 
 // Connection URL
 var url = 'mongodb://admin:pa22w0rd@ds062919.mlab.com:62919/mrj1ngles';
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
-    MongoClient.connect(url, function (err, db) {
-        assert.equal(null, err);
-        console.log("Connected correctly to server");
+    // 1 = load data from local file
+    // 2 = load data from database
+    var option = 2;
+    
+    switch (option) {
 
-        // Get the documents collection
-        var collection = db.collection('schlafattacken');
+        case 1:
+            fs.readFile("./InfoObjects.json", 'utf8', function (err, data) {
+                InfoObjects = JSON.parse(data);
+                console.log('GET index + Prizes');
+                res.render('index', {title: 'Home-Local', data: InfoObjects});
+            });
+            break;
 
-        // Insert some documents
+        case 2:
+            MongoClient.connect(url, function (err, db) {
+                assert.equal(null, err);
+                console.log("Connected correctly to server");
+
+                // Get the documents collection
+                var collection = db.collection('infoObjects');
+
+                // Insert some documents
 //        collection.insertMany([
 //            {a: 1}, {a: 2}, {a: 3}
 //        ], function (err, result) {
@@ -26,15 +46,21 @@ router.get('/', function (req, res, next) {
 //            console.log("Inserted 3 documents into the collection");
 //        });
 
-        // Find some documents
-        collection.find({ "person" : "Becks" }).toArray(function (err, docs) {
-            assert.equal(err, null);
-            console.log("Found the following records");
-            console.log(docs)
-            res.render('index', {title: 'Home', data: docs});
-        });
-        db.close();
-    });
+                // Find some documents
+                collection.find().toArray(function (err, docs) {
+                    assert.equal(err, null);
+                    console.log("Found the following records");
+                    console.log(docs)
+                    res.render('index', {title: 'Home-fromDB', data: docs});
+                });
+                db.close();
+            });
+            break;
+
+        default:
+    }
+
+
 });
 
 module.exports = router;
