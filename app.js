5b6ede6bffac69fbb1f86aa34a7fc8399dc569f3
bastib83 <bastib83@users.noreply.github.com>
2019-04-05 11:21:56 +0200

    building a countdown timer with socket.io
    https://robdodson.me/building-a-countdown-timer-with-socket-dot-io/

--- app.js
+++ app.js
@@ -50,4 +50,20 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
+var countdown = 1000;
+setInterval(function() {
+  countdown--;
+  io.sockets.emit('timer', { countdown: countdown });
+}, 1000);
+
+io.sockets.on('connection', function (socket) {
+  socket.on('reset', function (data) {
+    countdown = 1000;
+    io.sockets.emit('timer', { countdown: countdown });
+  });
+});
+
+
+
 module.exports = app;
